9d0da7c6d2e55d08b803725b461e1224c440f012
Devanand Yadav <devanandraj2002@gmail.com>
2022-09-15 11:48:29 +0530

    insertion at position method added

--- Linked List/doubly_linkedList.cpp
+++ Linked List/doubly_linkedList.cpp
@@ -40,16 +40,77 @@ public:
         return length;
     }
 
+    // insetion at head
+    void insertAtHead(Node* &head, int d) {
+        // 1. create a temp node with value d
+        Node* temp = new Node(d);
+        // 2.
+        temp->next = head;
+        // 3.
+        head->prev = temp;
+        // 4.
+        head = temp;
+    }
+
+    // insertion at tail
+    void insertAtTail(Node* &tail, int d) {
+        Node* temp = new Node(d);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+
+    // insert at poristion
+    void insertAtPosition(Node* &head, Node* &tail, int position, int d) {
+        if(position == 1) {
+            insertAtHead(head, d);
+            return ;
+        }
+
+        Node* temp = head;
+        int cnt = 1;
+        while(cnt<position-1){
+            temp = temp->next;
+            cnt++;
+        }
+
+        if(temp->next == NULL) {
+            insertAtTail(tail, d);
+            return ;
+        }
+
+        // create a new node with value d
+        Node* nodeToInsert = new Node(d);
+        nodeToInsert->next = temp->next;
+        temp->next->prev = nodeToInsert;
+        temp->next = nodeToInsert;
+        nodeToInsert->prev = temp;
+    }
 };
 
 
 int main()
 {
+    DoublyLinkedList dll;
     Node* node1 = new Node(10);
     Node* head = node1;
+    Node* tail = node1;
 
-    printLL(head);
+    dll.printLL(head);
 
+    dll.insertAtHead(head, 8);
+    dll.printLL(head);
 
-    cout<<"Length:\t"<<getLength(head)<<"\n";
+    dll.insertAtHead(head, 7);
+    dll.printLL(head);
+
+    dll.insertAtTail(tail, 22);
+    dll.printLL(head);
+
+    dll.insertAtTail(tail, 25);
+    dll.printLL(head);
+
+    dll.insertAtPosition(head, tail, 3, 9);
+    dll.printLL(head);
+
 }
